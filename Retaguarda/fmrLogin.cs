8146ad84107be0aa5a6bd0f25b820315d3631656
Luiz Antonio <luizantonio@metaposto.com.br>
2022-06-10 16:33:40 -0300

    Luiz Antonio - Criando os modelos

--- Retaguarda/fmrLogin.cs
+++ Retaguarda/fmrLogin.cs
@@ -16,5 +16,17 @@ namespace Retaguarda
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ValidarCampos();
+        }
+        private void ValidarCampos()
+        {
+            if (txtLogin.Text.Trim() == "" || txtSenha.Text.Trim() == "")
+            {
+                MessageBox.Show("Login ou senha não foram preenchidos");
+            }
+        }
     }
 }
